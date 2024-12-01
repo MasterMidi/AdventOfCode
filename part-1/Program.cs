5fd30262532ce945413c49fc568fd29e428472bf
Michael Andreas Graversen <home@michael-graversen.dk>
2024-12-01 14:12:28 +0100

    init: first day

---
+++ part-1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var lines = File.ReadLines(Path.Combine(Environment.CurrentDirectory, "input.txt"));
+
+var splitLines = lines.Select(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries)).ToList();
+
+var leftList = splitLines.Select(x => int.Parse(x[0].Trim())).Order();
+var rightList = splitLines.Select(x => int.Parse(x[1].Trim())).Order();
+
+var combinedList = leftList.Zip(rightList);
+
+var distances = combinedList.Select(row => row.First - row.Second).Select(Math.Abs);
+
+var totalDistance = distances.Sum();
+
+Console.WriteLine($"The total distance is: {totalDistance}");
---
+++ part-2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Drawing;
+
+var lines = File.ReadLines(Path.Combine(Environment.CurrentDirectory, "input.txt"));
+
+var splitLines = lines.Select(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries)).ToList();
+
+var leftList = splitLines.Select(x => int.Parse(x[0].Trim())).ToList();
+var rightList = splitLines.Select(x => int.Parse(x[1].Trim()));
+
+var similarityList = leftList.Select(num => rightList.Count(x => x == num));
+
+var totalSimilarity = leftList.Zip(similarityList).Select(row => row.First * row.Second).Sum();
+
+Console.WriteLine($"The total similarity is: {totalSimilarity}");
